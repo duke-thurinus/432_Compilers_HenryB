2ad148f1d542b443427a707763a3ae55053b141f
duke-thurinus <henry.balch1@marist.edu>
2021-03-04 19:34:43 -0500

    lex - Graph_vertex built

--- src/compiler.java
+++ src/compiler.java
@@ -1,9 +1,62 @@
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class compiler {
   public static void main(String[] args) {
     Char_stream stream = new Char_stream(System.in);
+    lex.lexer();
+  }
+}
+class lex{
+  static void lexer(){
+    Graph_vertex head = new Graph_vertex("");
+  }
+}
+class Graph_vertex{
+  String token;
+  HashMap<Character, Graph_vertex> vertices;
+
+  Graph_vertex(String token){
+    this.token = token;
+    vertices = new HashMap<>();
+  }
+  static void insert(Graph_vertex vertex, String word){
+    Graph_vertex lower_vertex = vertex.vertices.get(word.charAt(0));
+    if (lower_vertex == null){
+      if (word.length() == 1) {
+        vertex.vertices.put(word.charAt(0), new Graph_vertex(word));
+        // this is the end of insert and but last vertex is null
+      } else {
+        vertex.vertices.put(word.charAt(0), new Graph_vertex(""));
+        insert(vertex.vertices.get(word.charAt(0)), word, 1);
+        // next vertex is null but it is not the last
+      }
+    } else {
+      if (word.length() != 1){
+        insert(lower_vertex, word, 1);
+        // next vertex exists and there is more after
+      }
+    }
+  }
+
+  static void insert(Graph_vertex vertex, String word, int i){
+    Graph_vertex lower_vertex = vertex.vertices.get(word.charAt(i));
+    if (lower_vertex == null){
+      if (word.length() <= i+1) {
+        vertex.vertices.put(word.charAt(i), new Graph_vertex(word));
+        // this is the end of insert and but last vertex is null
+      } else {
+        vertex.vertices.put(word.charAt(i), new Graph_vertex(""));
+        insert(vertex.vertices.get(word.charAt(i)), word, i+1);
+        // next vertex is null but it is not the last
+      }
+    } else {
+      if (word.length() >= i+1){
+        insert(lower_vertex, word, i+1);
+        // next vertex exists and there is more after
+      }
+    }
   }
 }
 
