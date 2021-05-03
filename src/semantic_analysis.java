bd4b925f9e91114820c77f6ae75324b11acbcfaf
duke-thurinus <henry.balch1@marist.edu>
2021-05-03 16:55:43 -0400

    semantic_analysis - AST structure

--- src/semantic_analysis.java
+++ src/semantic_analysis.java
@@ -1,4 +1,8 @@
-public class semantic_analysis {
+public class semantic_analysis extends compiler{
+
+  static void analysis(){
+
+  }
 
   class AST{
     AST_node root;
@@ -8,6 +12,49 @@ public class semantic_analysis {
   }
 
   class AST_node{
+    String type;
+    int line_numb;
+    int line_pos;
+  }
 
+  class AST_block extends AST_node{
+    int cur_child;
+    AST_node[] children;
+
+    AST_block(){
+      type = grammar_block;
+      cur_child = 0;
+    }
+  }
+
+  class AST_var_declaration extends AST_node{
+    String var_name;
+    String var_type;
+
+    AST_var_declaration(String var_name, String var_type){
+      this.type = grammar_var_decl;
+      this.var_name = var_name;
+      this.var_type = var_type;
+    }
+  }
+
+  class AST_var_assign extends AST_node{
+    String var_name;
+    String var_type;
+
+    AST_var_assign(String var_name, String var_type){
+      this.type = grammar_assignment_statement;
+      this.var_name = var_name;
+      this.var_type = var_type;
+    }
+  }
+
+  class AST_print extends AST_node{
+    String var_name;
+
+    AST_print(String var_name){
+      this.type = grammar_print_statement;
+      this.var_name = var_name;
+    }
   }
 }
