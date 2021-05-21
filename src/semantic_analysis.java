53b5475291fc9625790f47d21851efcc5f2999ff
duke-thurinus <henry.balch1@marist.edu>
2021-05-20 23:39:02 -0400

    semantic_analysis - fixed tree generation bug with doubling children

--- src/semantic_analysis.java
+++ src/semantic_analysis.java
@@ -23,21 +23,25 @@ class AST_tree extends Syntax_tree {
     root.print_down_symbol_table(0);
   }
 
-  void add_node(String name){
+  void add_node(String name, int line_numb){
     if (root == null){
       //make root
-      root = new AST_node(name);
+      root = new AST_node(name, line_numb);
       current = root;
     } else {
       //new node
+      boolean success = false;
       for (int i=0; i < current.children.length; i++){
         if (current.children[i] == null){
-          current.children[i] = new AST_node(name, current);
+          current.children[i] = new AST_node(name, current, line_numb);
           current = current.children[i];
+          success = true;
           break;
         }
       }
-      current.add_to_full_children(name);
+      if (!success) {
+        current = current.add_to_full_children(name, line_numb);
+      }
     }
   }
 
@@ -52,15 +56,18 @@ class AST_node extends Syntax_tree_node{
   Symbol_table map = null;
   AST_node parent = null;
   AST_node[] children;
+  int line_numb;
 
-  AST_node(String name) {
+  AST_node(String name, int line_numb) {
     super(name);
+    this.line_numb = line_numb;
     children = new AST_node[5];
     if (name.equals(grammar_block)) map = new Symbol_table(find_scope() + 1);
   }
 
-  AST_node(String name, AST_node parent) {
+  AST_node(String name, AST_node parent, int line_numb) {
     super(name);
+    this.line_numb = line_numb;
     this.parent = parent;
     children = new AST_node[5];
     if (name.equals(grammar_block)) map = new Symbol_table(find_scope() + 1);
@@ -72,12 +79,13 @@ class AST_node extends Syntax_tree_node{
     return 0;
   }
 
-  void add_to_full_children(String name){
+  AST_node add_to_full_children(String name, int line_numb){
     int old_size = this.children.length;
     AST_node[] new_children = new AST_node[old_size*2];
     System.arraycopy(this.children, 0, new_children, 0, this.children.length);
     this.children = new_children;
-    this.children[old_size] = new AST_node(name, this);
+    this.children[old_size] = new AST_node(name, this, line_numb);
+    return this.children[old_size];
   }
 
   void analyze_down_tree(int depth){
